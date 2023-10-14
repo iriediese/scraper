56e53c162981001a0d577199f0878ac8ef266a52
Tedi <theodor@moise.dev>
2023-10-14 09:44:23 +0300

    finish backend

--- index.js
+++ index.js
@@ -7,78 +7,17 @@ const port = 3000
 
 const url = 'https://wsa-test.vercel.app/'
 
-const words = {
-  joyful: 2,
-  rewarding: 2,
-  happiness: 2,
-  positive: 1,
-  vibrant: 2,
-  encourage: 2,
-  enchanting: 2,
-  satisfaction: 1,
-  peace: 1,
-  effective: 2,
-  incredible: 2,
-  opportunities: 2,
-  support: 2,
-  green: 1,
-  calm: 1,
-  peaceful: 1,
-  beautiful: 1,
-  wonder: 2,
-  cherish: 2,
-  embrace: 2,
-  magnificent: 2,
-  cool: 1,
-  delightful: 2,
-  good: 1,
-  masterpiece: 2,
-  approval: 1,
-  negative: -1,
-  coping: -1,
-  rot: -2,
-  harmful: -2,
-  overwhelming: -2,
-  distress: -2,
-  expensive: -1,
-  pollution: -1,
-  stresses: -2,
-  challenge: -2,
-  challenges: -2,
-  negative: -1,
-  hefty: -1,
-  noise: -2,
-  bad: -1,
-  nonsensical: -2,
-  disapproval: -1,
-  disappointing: -2,
-  downside: -1,
-  obesity: -2,
-  'heart disease': -2,
-  diabetes: -2,
-  waste: -2,
-  harming: -2,
-  'not-so-rosy': -1
-}
+const words = require('./words.json')
 
-function getMood(text, flag) {
-  const tokens = text
-    .toLowerCase()
-    .split(/\s+/)
-    .map(word => word.replace(/[.,!?;:]+$/, ''))
-  let totalScore = 0
-  for (const token of tokens) {
-    totalScore += words[token] || 0
-  }
+app.get('/', async (req, res) => {
+  const cards = await getCards(url)
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify(cards, null, 2))
+})
 
-  if (totalScore > 0) {
-    return 'positive'
-  } else if (totalScore < 0) {
-    return 'negative'
-  } else {
-    return 'neutral'
-  }
-}
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
 
 async function getCards(url) {
   try {
@@ -86,70 +25,18 @@ async function getCards(url) {
       headless: 'new'
     })
     const page = await browser.newPage()
+
     await page.goto(url)
-
     await page.waitForSelector('div')
-
-    const content = await page.evaluate(() => {
-      const divElements = Array.from(document.querySelectorAll('div'))
-      const divsWithAAndDiv = []
-
-      divElements.forEach(div => {
-        const children = Array.from(div.children)
-        const hasA = children.some(child => child.tagName === 'A')
-        const hasDiv = children.some(child => child.tagName === 'DIV')
-
-        if (hasA && hasDiv) {
-          divsWithAAndDiv.push(div)
-        }
-      })
-
-      const completeObjects = []
-
-      divsWithAAndDiv.forEach(div => {
-        let title = ''
-        let description = ''
-        let image = ''
-        let href = ''
-        let date = ''
-
-        image = Array.from(div.querySelectorAll('img'))[0].getAttribute('src')
-        absouluteImageURL = new URL(image, window.location.href)
-        image = absouluteImageURL.toString()
-
-        date = Array.from(div.querySelectorAll('time'))[0].textContent.trim()
-
-        const aElementsWithSpan = Array.from(div.querySelectorAll('a > span'))
-        aElementsWithSpan.forEach(span => {
-          title = span.nextSibling.textContent.trim()
-          description = span.parentElement.parentElement.nextElementSibling.textContent.trim()
-          absoluteURL = new URL(span.parentElement.getAttribute('href'), window.location.href)
-          href = absoluteURL.toString()
-        })
-
-        completeObjects.push({
-          title: title,
-          short_description: description,
-          image: image,
-          href: href,
-          date: date
-        })
-      })
-
-      return completeObjects
-    })
+    const content = await page.evaluate(evaluateMainPage)
 
     for (c of content) {
       await page.goto(c.href)
       await page.waitForSelector('div > a')
-      c.mood = await page.evaluate(() => {
-        const backLink = Array.from(document.querySelectorAll('a'))[0]
-        const textDiv = Array.from(
-          backLink.parentElement.parentElement.querySelectorAll('div')
-        ).filter(div => div.querySelectorAll('*').length > 0)[0]
-        return textDiv.textContent
-      })
-      c.mood = getMood(c.mood, c.title === 'Neutral Observations on Modern Art')
+      c.sentiment = await page.evaluate(evaluateBlogPost)
+      moodResult = getMoodAndWordCount(c.sentiment)
+      c.sentiment = moodResult.sentiment
+      c.words = moodResult.words
     }
 
     await browser.close()
@@ -159,12 +46,88 @@ async function getCards(url) {
   }
 }
 
-app.get('/', async (req, res) => {
-  const cards = await getCards(url)
-  res.setHeader('Content-Type', 'application/json')
-  res.send(JSON.stringify(cards, null, 2))
-})
+function evaluateMainPage() {
+  const divElements = Array.from(document.querySelectorAll('div'))
+  const divsWithAAndDiv = []
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  divElements.forEach(div => {
+    const children = Array.from(div.children)
+    const hasA = children.some(child => child.tagName === 'A')
+    const hasDiv = children.some(child => child.tagName === 'DIV')
+
+    if (hasA && hasDiv) {
+      divsWithAAndDiv.push(div)
+    }
+  })
+
+  const completeObjects = []
+
+  divsWithAAndDiv.forEach(div => {
+    let title = ''
+    let description = ''
+    let image = ''
+    let href = ''
+    let date = ''
+
+    image = Array.from(div.querySelectorAll('img'))[0].getAttribute('src')
+    absouluteImageURL = new URL(image, window.location.href)
+    image = absouluteImageURL.toString()
+
+    date = Array.from(div.querySelectorAll('time'))[0].textContent.trim()
+
+    const aElementsWithSpan = Array.from(div.querySelectorAll('a > span'))
+    aElementsWithSpan.forEach(span => {
+      title = span.nextSibling.textContent.trim()
+      description = span.parentElement.parentElement.nextElementSibling.textContent.trim()
+      absoluteURL = new URL(span.parentElement.getAttribute('href'), window.location.href)
+      href = absoluteURL.toString()
+    })
+
+    completeObjects.push({
+      title: title,
+      short_description: description,
+      image: image,
+      href: href,
+      date: date
+    })
+  })
+
+  return completeObjects
+}
+
+function evaluateBlogPost() {
+  const backLink = Array.from(document.querySelectorAll('a'))[0]
+  const textDiv = Array.from(backLink.parentElement.parentElement.querySelectorAll('div')).filter(
+    div => div.querySelectorAll('*').length > 0
+  )[0]
+  return textDiv.textContent
+}
+
+function getMoodAndWordCount(text) {
+  const tokens = fixText(text)
+    .toLowerCase()
+    .split(/\s+/) // split on white space
+    .map(word => word.replace(/[.,!?;:]+$/, '')) // remove punctuation
+
+  let totalScore = 0
+  for (const token of tokens) {
+    totalScore += words[token] || 0
+  }
+
+  const result = {
+    sentiment: totalScore > 0 ? 'positive' : totalScore < 0 ? 'negative' : 'neutral',
+    words: tokens.length
+  }
+
+  return result
+}
+
+function fixText(text) {
+  // Add space between words without spaces (camelCased or PascalCased)
+  text = text.replace(/([a-z])([A-Z])/g, '$1 $2')
+
+  // Add space after a dot if there's no space after it
+  text = text.replace(/(\w)\.(?=\w)/g, '$1. ')
+
+  return text
+}
